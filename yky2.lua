30b571bb055b1e754c7a4449e3cac8c54e08de5b
yky5 <redcoolgg@gmail.com>
2025-10-17 20:00:01 -0700

    Update yky2.lua

--- yky2.lua
+++ yky2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/4b5a71a8dad384a070c03a4ad4bf0c12"))()
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/a7b1c9c1a2528dd8f74a216d55bbebae"))()
